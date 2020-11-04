3ae8f9ec8c1733e1a96ef06af948f4063a801837
Charles P <charles.prystupa@gmail.com>
2020-11-04 01:22:38 +0000

    Corrected files to include sync and async examples, along with npm test files for both

--- syncBreeds.js
+++ syncBreeds.js
@@ -1,21 +1,18 @@
-const fs = require('fs');
-
-const breedDetailsFromFile = function (breed, action) {
-  console.log('breedDetailsFromFile: Calling readFile...');
-
-  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
-    console.log("In readFile's Callback: it has the data...");
-    if (!error) action(data);
-  });
+// data in memory
+const catBreeds = {
+  'Balinese': "Balinese are curious, outgoing, intelligent cats with excellent communication skills. They are known for their chatty personalities and are always eager to tell you their views on life, love, and what you’ve served them for dinner.",
+  'Bombay': "The golden eyes and the shiny black coat of the Bombay is absolutely striking. Likely to bond most with one family member, the Bombay will follow you from room to room and will almost always have something to say about what you are doing, loving attention and to be carried around, often on his caregiver's shoulder."
 };
 
-// CHANGE 1: Moved the console.log into a new function:
-const printOutCatBreed = breed => {
-  console.log('Return Value: \n', breed) // => print out details correctly.
+// synchronous function to fetch a cat breed
+const breedDetails = function(breed) {
+  // can simply return it (easy peezee, butter squeezy) ...
+  return catBreeds[breed];
 };
 
-// CHANGE 2: we're now passing two arguments into breedDetailsFromFile: breed string and a callback function
-breedDetailsFromFile('Balinese', printOutCatBreed);
+// get the return value right away from the function
+const bombay = breedDetails('Bombay');
+// console.log(bombay); //=> prints out the description for that breed
 
 
-
+module.exports = breedDetails;
